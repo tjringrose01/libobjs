5f2ac0da7c9d9e93ab78ada3b037c374bbd45453
tjringrose01 <73766725+tjringrose01@users.noreply.github.com>
2023-03-09 15:58:35 -0500

    Update CurrencyAmount.cc

--- src/CurrencyAmount.cc
+++ src/CurrencyAmount.cc
@@ -1,3 +1,26 @@
+/**
+ *  CurrencyAmount.hh - Currency Class Definition - This class is responsible
+ *                      for defining a currency amount.
+ *
+ *  Copyright 2022 Timothy Ringrose
+ *
+ *  This file is part of libobjs.
+ *
+ *  libobjs is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libobjs is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libobjs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "CurrencyAmount.hh"
 #include <sstream>
 #include <iomanip>
@@ -22,6 +45,7 @@ void CurrencyAmount::setAmount(double amount) {
 }
 
 std::string CurrencyAmount::toString() const {
+    // Use a stringstream to build the string representation of the currency amount
     std::ostringstream oss;
     oss << std::fixed << std::setprecision(2) << amount_ << " " << currency_code_;
     return oss.str();
@@ -57,7 +81,42 @@ bool CurrencyAmount::operator!=(const CurrencyAmount& other) const {
     return !(*this == other);
 }
 
-/*bool CurrencyAmount::operator<(const CurrencyAmount& other) const {
+bool CurrencyAmount::operator<(const CurrencyAmount& other) const {
     // We can only compare currency amounts with the same currency code
-    if (currency_code_ !=
-*/
+    if (currency_code_ != other.currency_code_) {
+        throw std::invalid_argument("Currency codes do not match.");
+    }
+
+    // Compare the amounts
+    return amount_ < other.amount_;
+}
+
+bool CurrencyAmount::operator<=(const CurrencyAmount& other) const {
+    // We can only compare currency amounts with the same currency code
+    if (currency_code_ != other.currency_code_) {
+        throw std::invalid_argument("Currency codes do not match.");
+    }
+
+    // Compare the amounts
+    return amount_ <= other.amount_;
+}
+
+bool CurrencyAmount::operator>(const CurrencyAmount& other) const {
+    // We can only compare currency amounts with the same currency code
+    if (currency_code_ != other.currency_code_) {
+        throw std::invalid_argument("Currency codes do not match.");
+    }
+
+    // Compare the amounts
+    return amount_ > other.amount_;
+}
+
+bool CurrencyAmount::operator>=(const CurrencyAmount& other) const {
+    // We can only compare currency amounts with the same currency code
+    if (currency_code_ != other.currency_code_) {
+        throw std::invalid_argument("Currency codes do not match.");
+    }
+
+    // Compare the amounts
+    return amount_ >= other.amount_;
+}

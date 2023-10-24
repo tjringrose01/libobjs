78a8b7b011de1e9a08f1db719bb73c0a8cc47795
tjringrose01 <timothy.ringrose@outlook.com>
2023-10-23 21:36:33 -0400

    Removed automake stuff

--- autoclean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-rm -rf aclocal.m4 autom4te.cache/ compile config.status configure depcomp install-sh Makefile Makefile.in missing stamp-h config.log src/config.h src/config.h.in~ src/stamp-h1 src/Makefile src/Makefile.in src/.deps src/*.o src/libobjs
--- autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
- 
-autoheader -f
-touch NEWS README AUTHORS ChangeLog
-touch stamp-h
-aclocal
-autoconf -f
-automake --add-missing -c
